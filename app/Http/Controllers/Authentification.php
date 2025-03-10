fc2faa9f769d2474fa0edb16de73cdb1ec4a0bdb
HidDen-MaChinA <hei.franco.3@gmail.com>
2025-03-10 14:56:49 +0100

    feat: login method

---
+++ app/Http/Controllers/Authentification.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Mappers\User\GetUserMapper;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Http\Request;
+
+class Authentification extends BaseController
+{
+    use AuthorizesRequests, ValidatesRequests, HasUuids;
+    public function __construct(public GetUserMapper $getUserMapper)
+    {
+
+    }
+
+    public function login(Request $request){
+        $userLogin = $request->validate([
+            "email"=>"regex:\<|>\g, email",
+            "password"=>"password, regex:\<|>\g"
+        ]);
+        $authentified = auth()->attempt($userLogin);
+        if($authentified){
+            $currentUser = auth()->user();
+            return response()->json(["token"=> $currentUser->createToken($userLogin['email'] . $currentUser->getAuthIdentifier() . "_token")]);
+        }else{
+            return response()->json(["error" => "user not found"], 404);
+        }
+    }
+}
