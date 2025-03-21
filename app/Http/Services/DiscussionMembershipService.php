5e63bef18bb5637843a71216b0d3ff8caa6d6976
HidDen-MaChinA <hei.franco.3@gmail.com>
2025-03-21 07:16:31 +0100

    feat: create, update membership after user permission check

--- app/Http/Services/DiscussionMembershipService.php
+++ app/Http/Services/DiscussionMembershipService.php
@@ -6,11 +6,16 @@ use App\Models\Discussion;
 use App\Models\DiscussionsMembership;
 
 use exception;
+use Illuminate\Support\Facades\DB;
+
 use function Termwind\terminal;
 
 class DiscussionMembershipService{
     public function __construct() { }
     public function createDiscussionMembership($discussionId, $userId, $permission){
+        if(!$this->isMod($discussionId)){
+            throw new exception("only mod can perfom this action");
+        }
         $usedId = uuid_create();
         $toSave = new DiscussionsMembership([
             "id" => $usedId,
@@ -21,5 +26,27 @@ class DiscussionMembershipService{
         ]);
         $toSave->saveOrFail();
         return $toSave;
+    }
+
+    public function updateDiscussionMembershipPermission($discussionId, $userId, $permission){
+        if(!$this->isMod($discussionId)){
+            throw new exception("only mod can perfom this action");
+        }
+        return DiscussionsMembership::query()
+            ->where("discussion_id", $discussionId)
+            ->where("user_id", $userId)
+            ->update([
+                "permission" => $permission
+            ]);
+    }
+
+
+    private function isMod($discussionId){
+        $currentUser = auth()->user();
+        $userDiscussionMembership = DiscussionsMembership::query()
+            ->where("discussion_id", $discussionId)
+            ->where("user_id", $currentUser->id)
+            ->get();
+        return $userDiscussionMembership == 'mod';
     }
 }
